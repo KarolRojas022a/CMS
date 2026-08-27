b25e322bc2c79be5b5b68c0f71b301fb17a17b3d
KarolRojas022a <karolvivianarojasmoreno@gmail.com>
2026-08-27 15:55:52 -0500

    Completar rutas y vistas del proyecto

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (){
+    return view('pages.home');
+})->name('home');
+
+
+Route::get('/about', function(){
+ return view ('pages.about');
+
+})->name('about');
+
+
+Route::get('/contact', function(){
+ return view ('pages.contact');
+
+})->name('contact');
+
+
+
